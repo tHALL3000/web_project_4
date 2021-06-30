7076de055a756f25160212d420fd007d4cbadb3c
tHALL3000 <tiffanyhall.co@gmail.com>
2021-06-30 08:27:28 -0600

    Modal JS corrected. Border Radius is not working.

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,35 @@ let editBtn = document.querySelector("#editBtn");
 let saveBtn = document.querySelector("#saveBtn");
 let closeBtn = document.querySelector(".modal__close-bar");
 
-// Modal
-let toggleModal = () => {
-    document.querySelector(".modal").classList.toggle("modal--show");
-    document.querySelector(".overlay").classList.toggle("overlay__show");
-};
 let overlay = document.querySelector(".overlay");
-let modal = document.querySelector(".modal");
-
 // Text
 let profileName = document.querySelector(".profile__name");
 let profileTitle = document.querySelector(".profile__job");
+
 
 // Inputs
 let nameInput = document.forms.profile.elements.name;
 let titleInput = document.forms.profile.elements.title;
 
-function closeModal() {
-    overlay.classList.remove("overlay__show");
-    modal.classList.remove("modal--show");
-    profileName.textContent = nameInput.value;
-    profileTitle.textContent = titleInput.value;
+// Modal
+let toggleModal = () => {
+    if (!overlay.classList.contains("overlay__show")) { 
+    nameInput.value = profileName.textContent;
+    titleInput.value = profileTitle.textContent;
 }
+    document.querySelector(".overlay").classList.toggle("overlay__show");
+};
+
 
 editBtn.addEventListener("click", toggleModal);
-overlay.addEventListener("click", toggleModal);
-saveBtn.addEventListener("click", toggleModal);
 closeBtn.addEventListener("click", toggleModal);
+
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileTitle.textContent = titleInput.value;
+    toggleModal();
+}
+)
