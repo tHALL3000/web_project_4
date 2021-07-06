3df801f6b95a849338e44a222e6dd0fe1180a736
tHALL3000 <tiffanyhall.co@gmail.com>
2021-07-05 18:27:06 -0600

    Project 5 begins. Added versioning in footer. Ver 5.0 since it is the first commit for P5. removed hard-corded photo grid elements. added js array and function for the photo grid template. trash button is only placed but not styled.

--- scripts/index.js
+++ scripts/index.js
@@ -35,3 +35,51 @@ form.addEventListener("submit", (e) => {
     toggleModal();
 }
 )
+
+//Array for photo cards
+
+const initialCards = [
+{
+    name: "Cape Canaveral",
+    link: "https://images.unsplash.com/photo-1530447920184-b88c8872?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Fashion Capital",
+    link: "https://images.unsplash.com/photo-1618245613901-e52b7e0123c7?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTl8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Miami Beach",
+    link: "https://images.unsplash.com/photo-1622942817454-ed616e8d3a2d?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Neon Rain",
+    link: "https://images.unsplash.com/photo-1621870616319-eeb7fdf31234?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Land of Fire & Ice",
+    link: "https://images.unsplash.com/photo-1620053553156-92e15d54f7ee?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8eWVsbG93JTIwZmxhbWUlMjBibHVlJTIwc21va2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    name: "Silicon Valley",
+    link: "https://images.unsplash.com/photo-1621646912321-c97a233701d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=716&q=80"
+}
+];
+
+initialCards.forEach(data => {
+    //make card
+  const cardTemplate = document.querySelector(".card-template").content.querySelector(".photo-grid__card");
+  const cardElement = cardTemplate.cloneNode(true);
+  document.body.appendChild(cardElement); //unsure about this bit, w3 said this for div clone
+  const cardImage = cardElement.querySelector(".photo-grid__picture");
+  const cardTitle = cardElement.querySelector(".photo-grid__title");
+  const cardLike = cardElement.querySelector(".photo-grid__heart");
+  const cardDelete= cardElement.querySelector(".photo-grid__delete");
+
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+
+  const photoGrid = document.querySelector(".photo-grid")
+  photoGrid.prepend(cardElement);
+})
+
+
