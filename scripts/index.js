f5c75bb97ad0d601566e07a86c421e74cb74453d
tHALL3000 <tiffanyhall.co@gmail.com>
2021-07-09 16:21:07 -0600

    Grid photo is now in correct order v5.5

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,16 @@
 const editBtn = document.querySelector("#editBtn");
 const saveBtn = document.querySelector("#saveBtn");
 const photoModal = document.querySelector("#addPhoto");
+const imageModal = document.querySelector(".modal__type_add-card");
 
 //profile section
 const modalProfile = document.querySelector(".modal__type_edit-profile");
+// Text in profile
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__job");
+// Profile Inputs
+const nameInput = document.forms.profile.elements.name;
+const titleInput = document.forms.profile.elements.title;
 
 //overlays
 const overlay = document.querySelector(".overlay");
@@ -16,23 +23,15 @@ const profileClose = document.querySelector(".modal__close-bar");
 const addPhotoClose = document.querySelector(".modal__close-bar_add-photo");
 const imgPreviewClose = document.querySelector(".modal__close-bar_image");
 
+
+//enlarge picture section
+const modalPicture = document.querySelector(".modal__figure");
 const imageElement = document.querySelector(".modal__image");
 const imageCaption = document.querySelector(".modal__caption");
 
 
-const modalPicture = document.querySelector(".modal__figure");
 
 
-const imageModal = document.querySelector(".modal__type_add-card");
-
-// Text
-const profileName = document.querySelector(".profile__name");
-const proficonstitle = document.querySelector(".profile__job");
-
-
-// Profile Inputs
-const nameInput = document.forms.profile.elements.name;
-const titleInput = document.forms.profile.elements.title;
 
 // general Modal Popup function
 const toggleModal = (modal) => {
@@ -45,7 +44,6 @@ const toggleModal = (modal) => {
 
 
 // Modalfunction-for photo card
-
 const closeModal = (modal) => {
   modal.classlist.toggle("overlay_show");
 }
@@ -65,7 +63,7 @@ modal.addEventListener("click", () => {
 photoModal.addEventListener("click",  () => {
   toggleModal(addPhotoModal)
 });
-//photoModal.addEventListener("click", toggleModal(imgPreviewModal));
+
 
 //submit function for profile section
 const form = document.querySelector(".form");
@@ -74,35 +72,33 @@ form.addEventListener("submit", (e) => {
   profileName.textContent = nameInput.value;
   profileTitle.textContent = titleInput.value;
   toggleModal(modal__type_edit-profile);
-}
-);
+});
 
 //Array for photo cards- in reverse order, which is why we need the prepend not append
-
 const initialCards = [
+  {
+  name: "Silicon Valley",
+  link: "https://images.unsplash.com/photo-1621646912321-c97a233701d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=716&q=80"
+},
 {
-    name: "Cape Canaveral",
-    link: "https://images.unsplash.com/photo-1530447920184-b88c8872?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    name: "Fashion Capital",
-    link: "https://images.unsplash.com/photo-1618245613901-e52b7e0123c7?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTl8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    name: "Miami Beach",
-    link: "https://images.unsplash.com/photo-1622942817454-ed616e8d3a2d?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    name: "JS template!",
-    link: "https://images.unsplash.com/photo-1621870616319-eeb7fdf31234?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    name: "Look who made a ",
-    link: "https://images.unsplash.com/photo-1620053553156-92e15d54f7ee?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8eWVsbG93JTIwZmxhbWUlMjBibHVlJTIwc21va2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    name: "Silicon Valley",
-    link: "https://images.unsplash.com/photo-1621646912321-c97a233701d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=716&q=80"
+  name: "Miami Beach",
+  link: "https://images.unsplash.com/photo-1622942817454-ed616e8d3a2d?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+},
+{
+  name: "Land of Fire and Ice",
+  link: "https://images.unsplash.com/photo-1620053553156-92e15d54f7ee?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8eWVsbG93JTIwZmxhbWUlMjBibHVlJTIwc21va2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+},
+{
+  name: "Neon Rain",
+  link: "https://images.unsplash.com/photo-1621870616319-eeb7fdf31234?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+},
+{
+  name: "Fashion Capital",
+  link: "https://images.unsplash.com/photo-1618245613901-e52b7e0123c7?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTl8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+},
+{
+  name: "Cape Canaveral",
+  link: "https://images.unsplash.com/photo-1530447920184-b88c8872?ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
 }
 ];
 
