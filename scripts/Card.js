2250681ea285c1e42bf00001456b79f69654e055
tHALL3000 <tiffanyhall.co@gmail.com>
2021-08-10 15:36:27 -0600

    syntax and prettier

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,9 +1,3 @@
-//add image section
-// const pictureName = document.querySelector(".photo-grid__title");
-// const pictureLink = document.querySelector(".photo-grid__picture");
-
-// const addPhotoModal = document.querySelector(".overlay_type_add");
-
 const imageElement = document.querySelector(".modal__image");
 const imageCaption = document.querySelector(".modal__caption");
 
@@ -36,61 +30,55 @@ const handleOutsideClick = (e) => {
 
 
 class Card {
-    
-    
-    
+
     constructor(data, cardSelector) {
         this._name = data.name;
         this._link = data.link;
-        
-        this._cardSelector = cardSelector
-        
-        
+
+        this._cardSelector = cardSelector;
     }
+
     _getTemplate() {
-        
+
         const cardElement = this._cardSelector.content.querySelector(".photo-grid__card").cloneNode(true);
         return cardElement;
     }
-    
+
     _setEventListeners() {
         const cardLike = this._card.querySelector(".photo-grid__heart");
         const cardDelete = this._card.querySelector(".photo-grid__delete");
         const cardImage = this._card.querySelector(".photo-grid__picture");
-        
+
 
         cardLike.addEventListener("click", this._handleLikeIcon);
         cardDelete.addEventListener("click", () => this._handleDeleteIcon());
         cardImage.addEventListener("click", () => this._handlePreviewPicture());
     }
-    
+
     _handlePreviewPicture() {
         imageElement.src = this._link;
         imageElement.alt = "Image" + this._name + "";
         imageCaption.textContent = this._name;
         toggleModal(modalPreviewWindow);
-        
     }
-    
 
     _handleLikeIcon = () => {
-    this._card.querySelector(".photo-grid__heart").classList.toggle("photo-grid__heart_active")
-};
-
-    _handleDeleteIcon() {
-        this._card.remove()
+        this._card.querySelector(".photo-grid__heart").classList.toggle("photo-grid__heart_active")
     };
 
+    _handleDeleteIcon() {
+        this._card.remove();
+    };
 
     generateCard() {
-        this._card = this._getTemplate()
+        this._card = this._getTemplate();
         this._setEventListeners();
-        
+
         this._card.querySelector(".photo-grid__title").textContent = this._name;
         this._card.querySelector(".photo-grid__picture").src = this._link;
         this._card.alt = "Image" + this._name + "";
-        
+
         return this._card;
     }
 };
-    export default Card;
+export default Card;
--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -9,7 +9,7 @@ class FormValidator {
         console.log(formElement);
     };
 
-    
+
 
     _showErrorMessage(input) {
         const error = this._form.querySelector(`#${input.id}-error`);
@@ -18,31 +18,31 @@ class FormValidator {
         input.classList.add(this._inputErrorClass);
     }
     _hideErrorMessage(input) {
-    const error = this._form.querySelector(`#${input.id}-error`);
-    error.textContent = "";
-    error.classList.remove(this._errorClass);
-    input.classList.remove(this._inputErrorClass);
-};
+        const error = this._form.querySelector(`#${input.id}-error`);
+        error.textContent = "";
+        error.classList.remove(this._errorClass);
+        input.classList.remove(this._inputErrorClass);
+    };
 
 
     _checkInputValidity(input, button) {
-    if (input.validity.valid) {
-        this._hideErrorMessage(input, button);
-    } else {
-        this._showErrorMessage(input, button);
-    }
-};
+        if (input.validity.valid) {
+            this._hideErrorMessage(input, button);
+        } else {
+            this._showErrorMessage(input, button);
+        }
+    };
 
-    
+
     _toggleButtonState(inputs, button) {
-const isValid = this._inputs.every((input) => input.validity.valid);
-    if (isValid) {
-        this._button.classList.remove(this._inactiveButtonClass);
-        this._button.disabled = false;
-    } else {
-        this._button.classList.add(this._inactiveButtonClass);
-        this._button.disabled = true;
-    }
+        const isValid = this._inputs.every((input) => input.validity.valid);
+        if (isValid) {
+            this._button.classList.remove(this._inactiveButtonClass);
+            this._button.disabled = false;
+        } else {
+            this._button.classList.add(this._inactiveButtonClass);
+            this._button.disabled = true;
+        }
     };
 
     _setEventListeners() {
@@ -55,9 +55,9 @@ const isValid = this._inputs.every((input) => input.validity.valid);
                 //toggle button state
                 this._toggleButtonState(this._inputs, this._button);
             });
-        
-    
-})
+
+
+        });
     };
 
     enableValidation() {
@@ -66,26 +66,10 @@ const isValid = this._inputs.every((input) => input.validity.valid);
             this._button.classList.add(this._inactiveButtonClass);
             this._button.disabled = true;
         });
-    
+
         this._setEventListeners();
     };
 };
 
 
-
 export default FormValidator;
-
-
-
-// const editFormValidator = new FormValidator({})
-
-
-
-// {
-//     formSelector: ".form",
-//     inputSelector: ".modal__form-control-input",
-//     submitButtonSelector: ".button",
-//     inactiveButtonClass: "button-disabled",
-//     inputErrorClass: "modal__form-control-input-error",
-//     errorClass: "popup-error",
-// };
