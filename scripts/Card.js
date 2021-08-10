ac1727d9d7bd4ce0b100723e20ace987a7593479
tHALL3000 <tiffanyhall.co@gmail.com>
2021-08-10 09:32:41 -0600

    delete and preview not functional

--- scripts/Card.js
+++ scripts/Card.js
@@ -4,6 +4,9 @@
 
 // const addPhotoModal = document.querySelector(".overlay_type_add");
 
+const imageElement = document.querySelector(".modal__image");
+const imageCaption = document.querySelector(".modal__caption");
+
 const modalPreviewWindow = document.querySelector(".overlay_type_preview");
 
 const toggleModal = (modal) => {
@@ -30,11 +33,16 @@ const handleOutsideClick = (e) => {
     }
 };
 
+
+
 class Card {
+    
+    
+    
     constructor(data, cardSelector) {
         this._name = data.name;
         this._link = data.link;
-
+        
         this._cardSelector = cardSelector
         
         
@@ -44,32 +52,35 @@ class Card {
         const cardElement = this._cardSelector.content.querySelector(".photo-grid__card").cloneNode(true);
         return cardElement;
     }
-
-    _handleLikeIcon() {
-        cardLike.classList.toggle("photo-grid__heart_active");
-    }
-
-    _handleDeleteIcon() {
-        this._card.remove();
-    }
-
-    _handlePreviewPicture() {
-        imageElement.src = cardData.link;
-        imageElement.alt = "Image" + cardData.name + "";
-        imageCaption.textContent = cardData.name;
-        toggleModal(modalPreviewWindow);
-        
-    }
+    
     _setEventListeners() {
         const cardLike = this._card.querySelector(".photo-grid__heart");
         const cardDelete = this._card.querySelector(".photo-grid__delete");
         const cardImage = this._card.querySelector(".photo-grid__picture");
-    
+        
 
         cardLike.addEventListener("click", this._handleLikeIcon);
         cardDelete.addEventListener("click", this._handleDeleteIcon);
         cardImage.addEventListener("click", this._handlePreviewPicture);
     }
+    
+    _handlePreviewPicture() {
+        imageElement.src = this._link;
+        imageElement.alt = "Image" + this._name + "";
+        imageCaption.textContent = this._name;
+        toggleModal(modalPreviewWindow);
+        
+    }
+    
+
+    _handleLikeIcon = () => {
+    this._card.querySelector(".photo-grid__heart").classList.toggle("photo-grid__heart_active")
+};
+
+    _handleDeleteIcon = () => {
+        this._card.querySelector(".photo-grid__card").remove()
+    };
+
 
     generateCard() {
         this._card = this._getTemplate()
